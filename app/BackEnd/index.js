eb29db68485264a028f5b2592c56412c01a7a972
Abhishek Sharma <abhishek.webdev001@gmail.com>
2024-10-26 14:45:11 +0530

    API --testing-2

--- app/BackEnd/index.js
+++ app/BackEnd/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://next-artist.vercel.app"],
+    origin: [apiURL, "https://next-artist.vercel.app"],
   })
 );
 app.use(express.json());
