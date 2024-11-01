5deeabb94b8c112453be17111a499de4fb211f92
Abhishek Sharma <abhishek.webdev001@gmail.com>
2024-11-01 12:49:02 +0530

    After revert

--- app/Componants/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Loader = () => {
-  return (
-    <div className="h-[30px] w-[30px] rounded-[50%] bg-transparent border-zinc-200 border-t-zinc-700 border-8 animate-spin"></div>
-  );
-};
-
-export default Loader;
--- app/routes/Admin/Contacts/Contacts.jsx
+++ app/routes/Admin/Contacts/Contacts.jsx
@@ -3,7 +3,6 @@ import Masonry from "masonry-layout";
 import "./style.css";
 import { apiURL } from "@/app/constants";
 import ContactCard from "./Card/contactCard";
-import Loader from "@/app/Componants/Loader/Loader";
 
 const Contacts = () => {
   const [contacts, setContacts] = useState([]);
@@ -21,7 +20,7 @@ const Contacts = () => {
         setContacts(data.data);
         setFilteredContacts(data.data);
       }
-      setisLoading(false); // Set loading to false after data fetch
+      console.log(`From FrontEnd: `, data);
     } catch (error) {
       console.error("Error fetching contacts: ", error);
     }
@@ -71,15 +70,6 @@ const Contacts = () => {
     }
   }, [filteredContacts]);
 
-  const [isLoading, setisLoading] = useState(true);
-
-  useEffect(() => {
-    const handleLoad = () => setisLoading(false);
-    window.addEventListener("load", handleLoad);
-
-    return () => window.removeEventListener("load", handleLoad); // Clean up the event listener
-  }, []);
-
   return (
     <div className="contact-container">
       <div className="head">
@@ -95,19 +85,15 @@ const Contacts = () => {
         />
       </div>
       <hr className="hr" />
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <ul className="contact-grid">
-          {filteredContacts.length !== 0 ? (
-            filteredContacts.map((contact, index) => (
-              <ContactCard key={index} details={contact} />
-            ))
-          ) : (
-            <p>No contacts found.</p>
-          )}
-        </ul>
-      )}
+      <ul className="contact-grid">
+        {filteredContacts.length !== 0 ? (
+          filteredContacts.map((contact, index) => (
+            <ContactCard key={index} details={contact} />
+          ))
+        ) : (
+          <p>No contacts found.</p>
+        )}
+      </ul>
     </div>
   );
 };
