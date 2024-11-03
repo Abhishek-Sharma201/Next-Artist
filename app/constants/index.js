65887d89bb152423acfe3361d8869a5bbb9d92e5
Abhishek Sharma <abhishek.webdev001@gmail.com>
2024-11-03 22:30:06 +0530

    Dummy data -DELETED

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,21 +1,14 @@
 import {
-  IMG_,
   IMG_10,
   IMG_11,
   IMG_12,
-  IMG_13,
   IMG_14,
   IMG_15,
-  IMG_16,
   IMG_2,
   IMG_22,
   IMG_3,
-  IMG_37,
-  IMG_38,
-  IMG_39,
-  IMG_40,
-  IMG_7,
-  IMG_9,
+  IMG_30,
+  IMG_17,
 } from "../utils";
 
 export const apiURL = "https://next-artist-backend.onrender.com";
@@ -50,7 +43,7 @@ export const FeaturedCardSection = [
 export const myAlbumSectionsImgs = [
   {
     id: 1,
-    imgUrl: IMG_13,
+    imgUrl: IMG_30,
   },
   {
     id: 2,
@@ -66,152 +59,152 @@ export const myAlbumSectionsImgs = [
   },
 ];
 
-export const trendingProcuts = [
-  {
-    id: 1,
-    price: "₹1000",
-    text: "Customized ball pen sketch",
-    img: IMG_11,
-    metaData: {
-      id: 1,
-      price: "₹1000",
-      text: "Customized ball pen sketch",
-      img: IMG_11,
-    },
-  },
-  {
-    id: 2,
-    price: "₹1500",
-    text: "Customized graphic sketch",
-    img: IMG_,
-    metaData: {
-      id: 2,
-      price: "₹1500",
-      text: "Customized graphic sketch",
-      img: IMG_,
-    },
-  },
-  {
-    id: 3,
-    price: "₹1000",
-    text: "Water color sketch",
-    img: IMG_40,
-    metaData: {
-      id: 2,
-      price: "₹1000",
-      text: "Water color sketch",
-      img: IMG_40,
-    },
-  },
-  {
-    id: 4,
-    price: "₹1500",
-    text: "Charcoal and poster color sketch",
-    img: IMG_39,
-    metaData: {
-      id: 4,
-      price: "₹1500",
-      text: "Charcoal and poster color sketch",
-      img: IMG_39,
-    },
-  },
-  {
-    id: 5,
-    price: "₹1500",
-    text: "Couple face charcoal sketch",
-    img: IMG_38,
-    metaData: {
-      id: 5,
-      price: "₹1500",
-      text: "Couple face charcoal sketch",
-      img: IMG_38,
-    },
-  },
-  {
-    id: 6,
-    price: "₹1000",
-    text: "Ball pen sketch",
-    img: IMG_3,
-    metaData: {
-      id: 6,
-      price: "₹1000",
-      text: "Ball pen sketch",
-      img: IMG_3,
-    },
-  },
-  {
-    id: 7,
-    price: "₹1000",
-    text: "Water color sketch",
-    img: IMG_16,
-    metaData: {
-      id: 7,
-      price: "₹1000",
-      text: "Water color sketch",
-      img: IMG_16,
-    },
-  },
-  {
-    id: 8,
-    price: "₹1000",
-    text: "Water color sketch",
-    img: IMG_37,
-    metaData: {
-      id: 8,
-      price: "₹1000",
-      text: "Water color sketch",
-      img: IMG_37,
-    },
-  },
-];
+// export const trendingProcuts = [
+//   {
+//     id: 1,
+//     price: "₹1000",
+//     text: "Customized ball pen sketch",
+//     img: IMG_11,
+//     metaData: {
+//       id: 1,
+//       price: "₹1000",
+//       text: "Customized ball pen sketch",
+//       img: IMG_11,
+//     },
+//   },
+//   {
+//     id: 2,
+//     price: "₹1500",
+//     text: "Customized graphic sketch",
+//     img: IMG_,
+//     metaData: {
+//       id: 2,
+//       price: "₹1500",
+//       text: "Customized graphic sketch",
+//       img: IMG_,
+//     },
+//   },
+//   {
+//     id: 3,
+//     price: "₹1000",
+//     text: "Water color sketch",
+//     img: IMG_40,
+//     metaData: {
+//       id: 2,
+//       price: "₹1000",
+//       text: "Water color sketch",
+//       img: IMG_40,
+//     },
+//   },
+//   {
+//     id: 4,
+//     price: "₹1500",
+//     text: "Charcoal and poster color sketch",
+//     img: IMG_39,
+//     metaData: {
+//       id: 4,
+//       price: "₹1500",
+//       text: "Charcoal and poster color sketch",
+//       img: IMG_39,
+//     },
+//   },
+//   {
+//     id: 5,
+//     price: "₹1500",
+//     text: "Couple face charcoal sketch",
+//     img: IMG_38,
+//     metaData: {
+//       id: 5,
+//       price: "₹1500",
+//       text: "Couple face charcoal sketch",
+//       img: IMG_38,
+//     },
+//   },
+//   {
+//     id: 6,
+//     price: "₹1000",
+//     text: "Ball pen sketch",
+//     img: IMG_3,
+//     metaData: {
+//       id: 6,
+//       price: "₹1000",
+//       text: "Ball pen sketch",
+//       img: IMG_3,
+//     },
+//   },
+//   {
+//     id: 7,
+//     price: "₹1000",
+//     text: "Water color sketch",
+//     img: IMG_16,
+//     metaData: {
+//       id: 7,
+//       price: "₹1000",
+//       text: "Water color sketch",
+//       img: IMG_16,
+//     },
+//   },
+//   {
+//     id: 8,
+//     price: "₹1000",
+//     text: "Water color sketch",
+//     img: IMG_37,
+//     metaData: {
+//       id: 8,
+//       price: "₹1000",
+//       text: "Water color sketch",
+//       img: IMG_37,
+//     },
+//   },
+// ];
 
-export const bestSellerProcuts = [
-  {
-    id: 1,
-    price: "₹1000",
-    text: "Couple face charcoal sketch",
-    img: IMG_14,
-  },
-  {
-    id: 2,
-    price: "₹500",
-    text: "Single face charcoal sketch",
-    img: IMG_22,
-  },
-  {
-    id: 3,
-    price: "₹1000",
-    text: "Single face Water color sketch",
-    img: IMG_13,
-  },
-  {
-    id: 4,
-    price: "₹500",
-    text: "Single face graphite sketch",
-    img: IMG_7,
-  },
-  {
-    id: 5,
-    price: "₹500",
-    text: "Single face custom graphite sketch",
-    img: IMG_12,
-  },
-  {
-    id: 6,
-    price: "₹500",
-    text: "Custom graphite sketch",
-    img: IMG_9,
-  },
-  {
-    id: 7,
-    price: "₹1000",
-    text: "Handmade graphite sketch",
-    img: IMG_15,
-  },
-  {
-    id: 8,
-    price: "₹1000",
-    text: "Handmade graphite sketch",
-    img: IMG_10,
-  },
-];
+// export const bestSellerProcuts = [
+//   {
+//     id: 1,
+//     price: "₹1000",
+//     text: "Couple face charcoal sketch",
+//     img: IMG_14,
+//   },
+//   {
+//     id: 2,
+//     price: "₹500",
+//     text: "Single face charcoal sketch",
+//     img: IMG_22,
+//   },
+//   {
+//     id: 3,
+//     price: "₹1000",
+//     text: "Single face Water color sketch",
+//     img: IMG_13,
+//   },
+//   {
+//     id: 4,
+//     price: "₹500",
+//     text: "Single face graphite sketch",
+//     img: IMG_7,
+//   },
+//   {
+//     id: 5,
+//     price: "₹500",
+//     text: "Single face custom graphite sketch",
+//     img: IMG_12,
+//   },
+//   {
+//     id: 6,
+//     price: "₹500",
+//     text: "Custom graphite sketch",
+//     img: IMG_9,
+//   },
+//   {
+//     id: 7,
+//     price: "₹1000",
+//     text: "Handmade graphite sketch",
+//     img: IMG_15,
+//   },
+//   {
+//     id: 8,
+//     price: "₹1000",
+//     text: "Handmade graphite sketch",
+//     img: IMG_10,
+//   },
+// ];
