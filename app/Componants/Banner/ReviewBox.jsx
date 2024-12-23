333c76ae8b86b75b4df02892d7eda25209c1c856
Abhishek Sharma <abhishek.webdev001@gmail.com>
2024-12-23 15:21:35 +0530

    delete review

--- app/Componants/Banner/ReviewBox.jsx
+++ app/Componants/Banner/ReviewBox.jsx
@@ -56,6 +56,18 @@ const ReviewBox = ({ reviews, id }) => {
     }
   };
 
+  const handleDelete = async (id) => {
+    try {
+      const res = await fetch(`${apiURL}/api/review/deleteReview/${id}`);
+      if (!res) throw new Error("Error deleting review! from res.");
+      toast.warning("Review deleted!");
+      console.log(`Review deleted: ${id}`);
+    } catch (error) {
+      console.log(`Error deleted Review: ${id}`);
+      toast.error("Error deleting review!");
+    }
+  };
+
   const fakeReview = [
     {
       user: "test@1",
@@ -105,6 +117,7 @@ const ReviewBox = ({ reviews, id }) => {
                     fill="#fed255"
                     className="bi bi-trash"
                     viewBox="0 0 16 16"
+                    onClick={handleDelete(review._id)}
                   >
                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                     <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
