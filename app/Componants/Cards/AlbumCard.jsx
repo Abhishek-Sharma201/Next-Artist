a946df260f6812fb696391735dcc339cbd615cdc
Abhishek Sharma <abhishek.webdev001@gmail.com>
2025-01-14 15:58:12 +0530

    likes3

--- app/Componants/Cards/AlbumCard.jsx
+++ app/Componants/Cards/AlbumCard.jsx
@@ -1,10 +1,23 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./AlbumCard.css";
 import ShopBtn from "../Buttons/ShopBtn";
 
 const AlbumCard = ({ text, price, img, cardId, metaData, onShare, onLike }) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  useEffect(() => {
+    // Check if the drawing is already in localStorage
+    const existingLikes = JSON.parse(localStorage.getItem("likes")) || [];
+    setIsLiked(existingLikes.includes(cardId)); // Set isLiked to true if cardId is in localStorage
+  }, [cardId]);
+
+  const handleLike = () => {
+    onLike(cardId);
+    setIsLiked(!isLiked); // Toggle like status on local state change
+  };
+
   return (
     <div className="trendingCard">
       <div className="imgContainer">
@@ -22,17 +35,33 @@ const AlbumCard = ({ text, price, img, cardId, metaData, onShare, onLike }) => {
             </svg>
           </button>
           <div className="line"></div>
-          <button onClick={onLike}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-heart"
-              viewBox="0 0 16 16"
-            >
-              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-            </svg>
+          <button onClick={handleLike}>
+            {isLiked ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="gold"
+                className="bi bi-heart-fill"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-heart"
+                viewBox="0 0 16 16"
+              >
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+              </svg>
+            )}
           </button>
         </div>
         <Image
